bdce3e14378b8fc9c6cd4887dd05c4fd8dbf9612
jamaluddinbachapuram <jamaluddinbachapuram@gmail.com>
2023-12-25 10:29:25 +0000

    Solution

---
+++ src/components/HomeRoute/index.js
@@ -0,0 +1,21 @@
+import Header from '../Header'
+import Footer from '../Footer'
+import DiscountSlider from '../DiscountSlider'
+import RestaurantsList from '../RestaurantsList'
+import './index.css'
+
+const Home = () => (
+  <>
+    <Header />
+    <div className="main-resturant-container">
+      <div className="resturant-container">
+        <div className="desktop-offer-view">
+          <DiscountSlider />
+        </div>
+        <RestaurantsList />
+      </div>
+    </div>
+    <Footer />
+  </>
+)
+export default Home
